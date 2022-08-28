f7d9ada82398262d03d6f3c5906d9bdbad341f0c
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-08-28 16:28:04 +0200

    selecting elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
+///////////////     Functions ///////////////
+
+const nextSilde = () => {
+  console.log(`rawan`);
+};
+
+const prevSilde = () => {
+  console.log(`waten`);
+};
+
+/////////////////////////////////////////  Selecting Elements   ///////////////////////////////////////
 // get slider items, Array.from()[ES6 feature]
 var sliderImages = Array.from(
   document.querySelectorAll(`.slider-container img`)
 );
-console.table(sliderImages);
+
+// get number of slides
+let slidesCount = sliderImages.length;
+
+// set currnet slide
+let currnetSlide = 1;
+
+// slide number element
+let slideNumberElement = document.getElementById(`slide-number`);
+
+// previous and next elements
+let nextButton = document.getElementById(`next`);
+let prevButton = document.getElementById(`prev`);
+
+///////////////////////////////////////    Handling Events   //////////////////////////////////////////////
+// Handling clicks on next and previous buttons
+nextButton.onclick = nextSilde;
+prevButton.onclick = prevSilde;
