9cad8bd948a88429802a496322cc1139c8d50d0c
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-08-28 17:26:49 +0200

    doing the pagination with the javescript only

--- js/main.js
+++ js/main.js
@@ -27,6 +27,30 @@ let slideNumberElement = document.getElementById(`slide-number`);
 let nextButton = document.getElementById(`next`);
 let prevButton = document.getElementById(`prev`);
 
+// Create the main ul element
+let paginationElement = document.createElement(`ul`);
+
+// Set attribute to the created ul
+paginationElement.setAttribute(`id`, `pagination-ul`);
+
+// Create list items based on slides count
+for (let i = 1; i <= slidesCount; i++) {
+  // create the list item
+  let paginationItem = document.createElement(`li`);
+
+  // Set attribute to custome li
+  paginationItem.setAttribute(`data-index`, i);
+
+  //set item content
+  paginationItem.appendChild(document.createTextNode(i));
+
+  //append items to the main ul list
+  paginationElement.appendChild(paginationItem);
+}
+
+//Add the created ul to the page
+document.getElementById(`indicators`).appendChild(paginationElement);
+
 ///////////////////////////////////////    Handling Events   //////////////////////////////////////////////
 // Handling clicks on next and previous buttons
 nextButton.onclick = nextSilde;
